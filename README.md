472c6f809ef349b526f27683a462e37b0397e8cf
Branislav Blazek <branislav.blazek.bb@gmail.com>
2021-04-30 01:11:24 +0200

    README.md and first accelerometer file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # micro-bit
 Some interesants scripts for doing fun with micro:bit (V2)
+
+# online set up
+open https://python.microbit.org/v/2
+-> Connect
+-> Download
+-> Load/Save and choose some .py file
+-> Flash
+
+# accelerometer
+accelerometer_basic.py just shows X or Y values via LED 
+
---
+++ accelerometer_basic.py
@@ -0,0 +1,57 @@
+# Add your Python code here. E.g.
+from microbit import *
+ 
+def write_led_code(input_code, dire = 'x', base = '', rev = False):
+    code = ''.join(reversed(input_code)) if rev else input_code 
+    
+    final_string = ''
+    for i_row in range(5):
+        line = ''
+        for i_col in range(5):
+            olds = None
+            a = i_row
+            b = i_col
+            if dire == 'y':
+                a = i_col
+                b = i_row
+            news = code[b]
+
+            if base != '':
+                olds = base[(b * 5 + b ** 1) + a]
+                if code[b] == '0':
+                    news = olds
+            line += news
+
+        line += ':'
+        final_string += line
+    return final_string
+    
+def calc_value(val, typ = 'x'):
+    reverse = val < 0
+    value = abs(val)
+    code = None
+    codes = ['00003', '00033', '00036', '00066', '00069', '00099']
+    
+    if value >= 50:
+        if value <= 100:
+            code = codes[0]
+        elif value <= 200:
+            code = codes[1]
+        elif value <= 400:
+            code = codes[2]
+        elif value <= 600:
+            code = codes[3]
+        elif value <= 800:
+            code = codes[4]
+        elif value >= 800:
+            code = codes[5]
+            
+    if code != None:
+        return write_led_code(code, dire=typ, rev=reverse)
+    return "00300:00600:36963:00600:00300"
+ 
+while True:
+    acc_x = accelerometer.get_x()
+    acc_y = accelerometer.get_y()
+    data = calc_value(acc_y, 'y')
+    display.show(Image(data))
